f1adf1ff9cfedef22dcb3445759aab04a5510c4f
opestudio <hello.ope21@gmail.com>
2017-05-17 19:23:04 +0700

    update

--- application/libraries/UniqueCode.php
+++ application/libraries/UniqueCode.php
@@ -11,6 +11,7 @@ class Uniquecode
     $this->CI =& get_instance();
 
     $product_code = '';
+    $numb = '0000';
 
     // cek last id kode
     $query = "SELECT MAX(last_id) AS id, id_code FROM tb_uniq_code WHERE code_name='$str'";
@@ -19,7 +20,19 @@ class Uniquecode
     $id = $result['data'][0]['id'];
     $code = $result['data'][0]['id_code'];
 
-    $unique = $code.$code+1.'000'.$id+1;
+    // update last id
+    $this->CI->dbm->set_table('tb_last_kode');
+    $data_update = array(
+      'last_id' => $id+1
+    );
+
+    $update = $this->CI->dbm->update($data_update);
+    $result = json_decode($cek, TRUE);
+    $result_code = $result['result_code'];
+
+    if ($result_code == 0) {
+      return $product_code . substr($numb,0,-strlen($id)) . $id;
+    }
 
   }
 }
