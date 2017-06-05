0009551c6f51bdb991cc4c616e13ee7610c841f1
akaope <hello.ope21@gmail.com>
2017-06-05 18:28:29 +0700

    update

---
+++ application/logs/log-2017-06-04.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-06-04 12:26:32 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: NO) /var/www/html/perpusApp/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2017-06-04 12:26:32 --> Unable to connect to the database
